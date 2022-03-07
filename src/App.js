c34ebc675d8079afea928e9e2894645c455c20bb
obaloluwa28 <oduyemi.obaloluwa@gmail.com>
2022-03-07 21:11:38 +0100

    added dashboard content

--- src/App.js
+++ src/App.js
@@ -2,14 +2,22 @@ import React from 'react'
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import './App.css';
 import Sidebar from './Components/Sidebar/Sidebar';
+import Dashboard from './Pages/Dashboard/dashboard';
 
 const App = () => {
   return (
-  <Router>
-    <Routes>
-      <Route exact path="/" element={<Sidebar />} />            
-    </Routes>
-  </Router>
+    <Router>
+      <div className="appcontainer">
+        <div className="sbrcontainer">
+          <Sidebar />  
+        </div>
+        <div className="rightContainer">
+          <Routes>
+            <Route exact path="/" element={<Dashboard />} />            
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
--- src/Pages/Dashboard/dashboard.js
+++ src/Pages/Dashboard/dashboard.js
@@ -4,7 +4,24 @@ import './dashboard.css'
 const Dashboard = () => {
   return (
     <div className="main-container">
+        <div className="left-container">
+            <div className="left-top-contain">
+                <div className="LTC-left">
+                    <div className="LTC-right-content">
+                        
+                    </div>
+                </div>
+                <div className="LTC-right">
+                    <div className="LTC-right-content">
 
+                    </div>
+                </div>
+            </div>
+            <div className="left-bottom-contain">Bottom</div>
+        </div>
+        <div className="right-container">
+            rightright
+        </div>
     </div>
   )
 }
